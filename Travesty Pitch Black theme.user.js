00076e9dba5e1994ba24221c962a48ebf1e44ba3
Mythical Trashcan <stardust5047@gmail.com>
2025-02-21 15:40:18 -0500

    Update pitch black theme to have borders

--- Travesty Pitch Black theme.user.js
+++ Travesty Pitch Black theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Travesty Pitch Black theme
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Pitch black theme for Travesty
 // @author       1_Mythical
 // @match        https://alpha.travesty.chat/*
@@ -25,16 +25,17 @@
     document.documentElement.style.setProperty("--chakra-colors-brandGray-light", "#000000");
     document.documentElement.style.setProperty("--chakra-colors-brandGray-dark", "#000000");
     document.documentElement.style.setProperty("--chakra-colors-brandGray-active", "#000000");
-    document.documentElement.style.setProperty("--chakra-colors-brandGreen", "#000000");
     document.documentElement.style.setProperty("--chakra-colors-brandGray-hover", "#000000");
     document.documentElement.style.setProperty("--chakra-colors-brandGray-accent", "#8b8b8b");
+    document.documentElement.style.setProperty("--chakra-colors-brandGreen", "#01532d");
+    document.documentElement.style.setProperty("--chakra-colors-menuRed", "#730000");
 
     const style = document.createElement('style');
     style.innerHTML = `
         body {
             background-color: #000000;
         }
-            
+
         .css-9udf7b::-webkit-scrollbar-track {
             background-color: rgb(0, 0, 0);
         }
@@ -42,6 +43,64 @@
         .css-1gbighu::-webkit-scrollbar-track {
             background-color: rgb(0, 0, 0);
         }
+
+        .css-8r9gla {
+            border-width: 1px;
+            border-top: 0px;
+            border-left: 0px;
+            border-color: #393939;
+        }
+
+        .css-z6wft2 {
+            border-width: 1px;
+            border-top: 0px;
+            border-bottom: 0px;
+            border-left: 0px;
+            border-color: #393939;
+        }
+
+        .css-164bh9z {
+            border-width: 1px;
+            border-top: 0px;
+            border-bottom: 0px;
+            border-left: 0px;
+            border-color: #393939;
+        }
+
+        .css-174u0o2 {
+            border-width: 1px;
+            border-top: 0px;
+            border-bottom: 0px;
+            border-left: 0px;
+            border-color: #393939;
+        }
+
+        .css-1vtlm7k {
+            border-width: 1px;
+            border-top: 0px;
+            border-bottom: 0px;
+            border-right: 0px;
+            border-color: #393939;
+        }
+
+        .css-1x016yl {
+            border-width: 1px;
+            border-top: 0px;
+            border-bottom: 0px;
+            border-left: 0px;
+            border-color: #393939;
+        }
+
+        .css-1nnh735 {
+            border-width: 1px;
+            border-top: 0px;
+            border-left: 0px;
+            border-right: 0px;
+            border-color: #393939;
+        }
+
+        .css-11pna72 {
+        box-shadow: #393939 0px 0px 0px 1px;
     `;
     document.head.appendChild(style);
 })();
